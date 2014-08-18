37b59dd91961849617ff57f9291be259efcf61ca
jimmynotjim <jimmynotjim@me.com>
2014-08-17 21:21:28 -0400

    Fixed missing comma

--- themes/evolved-child-theme/Gruntfile.js
+++ themes/evolved-child-theme/Gruntfile.js
@@ -105,7 +105,7 @@ module.exports = function(grunt) {
     sass: {
       options: {
         loadPath: '<%= BOWER_DIR %>'
-      }
+      },
       dist: {
         options: {
           style:        'compressed',
