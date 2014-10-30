286ea97773c79da9d286b67f7b0bc6b34f42fb80
jimmynotjim <jimmynotjim@me.com>
2014-10-30 11:16:56 -0400

    Fixed incorrect EJS templating

--- root/Gruntfile.js
+++ root/Gruntfile.js
@@ -191,7 +191,7 @@ module.exports = function(grunt) {
           livereload: true
         },
         files: [
-          '<%%= CHILD_DIR %>/**'
+          '<%= CHILD_DIR %>/**'
         ]
       }
     }
